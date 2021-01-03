1fb1cca4a1ef45f5fc3c46cc31431f3322eacf1e
Murat Aşık <muratasik7@gmail.com>
2021-01-03 19:11:24 +0300

    Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_todo_list/screens/todo_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todos Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoList(),
+    );
+  }
+}
